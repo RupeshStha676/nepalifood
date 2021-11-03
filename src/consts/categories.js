ebb89bb798985008b1d7fe964d4b0aa374c8b044
Rupesh Shrestha <s335834@students.cdu.edu.au>
2021-11-03 20:27:40 +0930

    major changes

---
+++ src/consts/categories.js
@@ -0,0 +1,7 @@
+const categories = [
+    {id: '1', name: 'chowmein', image: require('../assets/catergories/chowmein.png')},
+    {id: '2', name: 'momo', image: require('../assets/catergories/momo.png')},
+    {id: '3', name: 'naan', image: require('../assets/catergories/naan.png')},
+];
+  
+export default categories;
---
+++ src/consts/foods.js
@@ -0,0 +1,46 @@
+const foods = [
+    {
+      id: '1',
+      name: 'Veg Chowmein',
+      ingredients: 'Vegetables',
+      price: '7.50',
+      image: require('../assets/vegchowmein.png'),
+    },
+    {
+      id: '2',
+      name: 'Chicken Chowmein',
+      ingredients: 'Chicken',
+      price: '8.50',
+      image: require('../assets/chickenchowmein.png'),
+    },
+    {
+      id: '3',
+      name: 'Veg Momo',
+      ingredients: 'Vegetables',
+      price: '9.50',
+      image: require('../assets/vegmomo.png'),
+    },
+    {
+      id: '4',
+      name: 'Buff Momo',
+      ingredients: 'Buffalo Meat',
+      price: '11.50',
+      image: require('../assets/buffmomo.png'),
+    },
+    {
+        id: '5',
+        name: 'Plain naan',
+        ingredients: 'Flour',
+        price: '4.50',
+        image: require('../assets/plainnaan.png'),
+      },
+    {
+        id: '6',
+        name: 'Butter Naan',
+        ingredients: 'Butter',
+        price: '5.50',
+        image: require('../assets/butternaan.png'),
+    },
+  ];
+  
+  export default foods;
